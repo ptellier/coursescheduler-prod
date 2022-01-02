d83f604a71b287e9db887a4202f5e4fcfc35d19a
limseung1 <54563657+limseung1@users.noreply.github.com>
2022-01-02 15:04:12 +0900

    Add files via upload

--- src/helpers/fetch.ts
+++ src/helpers/fetch.ts
@@ -1,14 +1,33 @@
 import { SearchWord } from "../data/DataDefinition/SearchWordDD";
-
-// Functions in this file fetches data from Course API 
+import { Section } from "../data/DataDefinition/SectionDD";
+// Functions in this file fetches data from Course API
 
 /**
  * fetches sections that corresponds to given sw
  * @param sw; i.e CPSC/110
  */
-export const fetchSection = async(sw: SearchWord) => {
-    const url = `https://api.ubccourses.com/section/${sw}/?realtime=1`
-    const res = await fetch(url)
-    const data = await res.json()
-    return data
-}
+export const fetchSection = async (sw: SearchWord) => {
+  const url = `https://api.ubccourses.com/section/${sw}/?realtime=1`;
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
+};
+
+/**
+ * fetch and filter for available sections
+ * @param {SearchWord[]} losw
+ * @returns {Section[]}
+ */
+export const fetchSections = async (losw: SearchWord[]): Promise<Section[]> => {
+  let acc: Section[] = [];
+  for (let sw of losw) {
+    const data = await fetchSection(sw);
+    acc.push(...data.sections);
+
+    // const sections_avail = filterAvailSections(data.sections);
+    // acc.push(...sections_avail);
+  }
+  return acc;
+};
+
+
---
+++ src/helpers/groupby.ts
@@ -0,0 +1,38 @@
+import React from "react";
+import { Section } from "../data/DataDefinition/SectionDD";
+
+/**
+ * create subgroups of sections by course and activity
+ * @example
+ * [
+ *  [110Lectures], 
+ *  [110Labs], 
+ *  [121lectures...], 
+ *  [121labs...], 
+ *  [121tutorials...]
+ * ]
+ * @param los 
+ * @returns 
+ */
+ export const groupSections = (los: Section[]): Section[][] => {
+    const result = groupBy(los, function (section:Section) {
+        return [section.course, section.activity];
+    })
+    return result
+}
+
+/**
+ * group array into several sections by given conditions, fn
+ * @param array 
+ * @param f 
+ * @returns 
+ */
+const groupBy = (array:Section[], f: Function) => {
+    let groups:any = {};
+    array.forEach(function (o) {
+        let group = JSON.stringify(f(o));
+        groups[group] = groups[group] || [];
+        groups[group].push(o);
+    });
+    return Object.keys(groups).map(group => groups[group])
+}
