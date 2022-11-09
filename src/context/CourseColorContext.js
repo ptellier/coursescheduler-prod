cf465d687e3c9f92d7dd998a7af75ed9ebb02192
Seungwon Lim <limcolby1@gmail.com>
2022-11-08 21:34:42 -0800

    refactored: Course Colors use stack to track colors

--- src/context/CourseColorContext.js
+++ src/context/CourseColorContext.js
@@ -3,32 +3,45 @@ import { red, pink, green, lime, lightGreen, lightBlue, grey, purple, yellow, or
 
 export const CourseColorContext = createContext();
 
-const colors = [red[900], lightBlue[900], green[900], purple[900], orange[900], brown[900], teal[900]]
-const backgroundColors = [red[100], lightBlue[100], green[100], purple[100], orange[100], brown[100], teal[100]]
+let colors = [red[900], lightBlue[900], green[900], purple[900], orange[900], brown[900], teal[900]]
+let backgroundColors = [red[100], lightBlue[100], green[100], purple[100], orange[100], brown[100], teal[100]]
+
+let availableColors = [
+    {"color":red[900], "backgroundColor":red[100]},
+    {"color":lightBlue[900], "backgroundColor":lightBlue[100]},
+    {"color":green[900], "backgroundColor":green[100]},
+    {"color":purple[900], "backgroundColor":purple[100]},
+    {"color":orange[900], "backgroundColor":orange[100]},
+    {"color":brown[900], "backgroundColor":brown[100]},
+    {"color":teal[900], "backgroundColor":teal[100]},
+ ]
+let usedColors = []
 
 export const CourseColorProvider = (props) => {
 
     const [courseColors, setCourseColors] = useState({})
-    const [n, setN] = useState(0)
 
     const addCourseColor = (courseName) => {
         let addingCourseColor = {}
-        addingCourseColor[courseName] = {"color":colors[n], "backgroundColor":backgroundColors[n]}
+        const c = availableColors.pop()
+        usedColors.push(c)
+        addingCourseColor[courseName] = c
         setCourseColors(courseColors => ({
             ...courseColors,
             ...addingCourseColor
         }))
-        setN(n + 1)
+        // setN(n + 1)
     }
 
     const removeCourseColor = (courseName) => {  
         if (courseName in courseColors) {
+            const c = usedColors.pop()
+            availableColors.push(c)
             let copiedCourseColors = {...courseColors}
             delete copiedCourseColors[courseName]
             setCourseColors(courseColors => ({
                 ...copiedCourseColors,
             }))
-            setN(n - 1)
         }
     }
 
